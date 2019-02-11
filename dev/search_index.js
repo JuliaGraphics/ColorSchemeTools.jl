d37f904adf0db3088c4cce31ce6ab66014487dfc
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-02-11 10:12:30 +0000

    build based on 2a7f216

--- dev/search_index.js
+++ dev/search_index.js
@@ -117,7 +117,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Converting image colors",
     "title": "Convert image from one scheme to another",
     "category": "section",
-    "text": "Using the ColorSchemes function getinverse() it\'s possible to convert an image using one colorscheme to use another.convert_to_scheme(cscheme, img) returns a new image in which each pixel from the provided image is mapped to its closest matching color in the provided scheme.In the following figure, the Julia logo is converted to use a ColorScheme with no black or white:using FileIO, ColorSchemes, ColorSchemeTools, Images\n\nimg = load(\"julia-logo-square.png\")\nimg_rgb = RGB.(img) # get rid of alpha channel\nconvertedimage = convert_to_scheme(ColorSchemes.PiYG_4, img_rgb)\n\nsave(\"original.png\",  img)\nsave(\"converted.png\", convertedimage)(Image: \"julia logo converted\")convert_to_scheme"
+    "text": "It\'s possible to convert an image using one color scheme to use another.convert_to_scheme(cscheme, img) returns a new image in which each pixel from the provided image is mapped to its closest matching color in the provided scheme. See ColorSchemes\'s getinverse() function for more details on how this works.In the following figure, the Julia logo is converted to use a ColorScheme with no black or white:using FileIO, ColorSchemes, ColorSchemeTools, Images\n\nimg = load(\"julia-logo-square.png\")\nimg_rgb = RGB.(img) # get rid of alpha channel\nconvertedimage = convert_to_scheme(ColorSchemes.PiYG_4, img_rgb)\n\nsave(\"original.png\",  img)\nsave(\"converted.png\", convertedimage)(Image: \"julia logo converted\")Notice how the white was matched by the color right at the boundary of the light purple and pale green.convert_to_scheme"
 },
 
 {
